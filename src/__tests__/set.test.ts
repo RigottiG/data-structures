7d1200841486473e6b415f0ce9a486fbaa1f16a4
Guilherme Rigotti <ggrigotti@gmail.com>
2023-01-18 19:56:39 -0300

    feat: implements the set data structure

---
+++ src/__tests__/set.test.ts
@@ -0,0 +1,86 @@
+import { Set } from '../structures/set';
+
+let set: Set<string>;
+
+describe('Set', () => {
+  beforeEach(() => {
+    set = new Set<string>();
+  });
+
+  it('should add elements to the set', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    expect(set.items).toEqual({ a: 'a', b: 'b', c: 'c' });
+  });
+
+  it('should return true if the element is added to the set', () => {
+    expect(set.add('a')).toBe(true);
+  });
+
+  it('should return false if the element is not added to the set', () => {
+    set.add('a');
+    expect(set.add('a')).toBe(false);
+  });
+
+  it('should return true if the element is deleted from the set', () => {
+    set.add('a');
+    expect(set.delete('a')).toBe(true);
+  });
+
+  it('should return false if the element is not deleted from the set', () => {
+    expect(set.delete('a')).toBe(false);
+  });
+
+  it('should return true if the element is in the set', () => {
+    set.add('a');
+    expect(set.has('a')).toBe(true);
+  });
+
+  it('should return false if the element is not in the set', () => {
+    expect(set.has('a')).toBe(false);
+  });
+
+  it('should clear the set', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    set.clear();
+
+    expect(set.items).toEqual({});
+  });
+
+  it('should return the size of the set', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    expect(set.size()).toBe(3);
+  });
+
+  it('should return the size of the set using a legacy method', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    expect(set.sizeLegacy()).toBe(3);
+  });
+
+  it('should return the values of the set', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    expect(set.values()).toEqual(['a', 'b', 'c']);
+  });
+
+  it('should return the values of the set using a legacy method', () => {
+    set.add('a');
+    set.add('b');
+    set.add('c');
+
+    expect(set.valuesLegacy()).toEqual(['a', 'b', 'c']);
+  });
+});
--- src/structures/set.ts
+++ src/structures/set.ts
@@ -4,4 +4,60 @@ export class Set<T> {
   constructor() {
     this.items = {};
   }
+
+  has(element): boolean {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  add(element): boolean {
+    if (this.has(element)) {
+      return false;
+    }
+
+    this.items[element] = element;
+    return true;
+  }
+
+  delete(element): boolean {
+    if (!this.has(element)) {
+      return false;
+    }
+
+    delete this.items[element];
+    return true;
+  }
+
+  clear(): void {
+    this.items = {};
+  }
+
+  size(): number {
+    return Object.keys(this.items).length;
+  }
+
+  sizeLegacy(): number {
+    let count = 0;
+    for (const key in this.items) {
+      if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  values(): T[] {
+    return Object.values(this.items);
+  }
+
+  valuesLegacy(): T[] {
+    const values: T[] = [];
+    for (const key in this.items) {
+      if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+        values.push(this.items[key]);
+      }
+    }
+
+    return values;
+  }
 }
