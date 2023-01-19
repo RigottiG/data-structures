5e68dba40f142f8618c3ea84c88013b3e6ce46ff
Guilherme Rigotti <ggrigotti@gmail.com>
2023-01-19 11:50:57 -0300

    feat: add new functions to dictionary
    it was added new functions and the related tests

--- src/__tests__/dictionary.test.ts
+++ src/__tests__/dictionary.test.ts
@@ -42,4 +42,94 @@ describe('Dictionary', () => {
   it('should return false if value is not provided', () => {
     expect(dictionary.set('key', null)).toBeFalsy();
   });
+
+  it('should return all keys', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    expect(dictionary.keys()).toEqual(['key1', 'key2', 'key3']);
+  });
+
+  it('should return all values', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    expect(dictionary.values()).toEqual([1, 2, 3]);
+  });
+
+  it('should return all key value pairs', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    expect(dictionary.keyValues()).toEqual([
+      { key: 'key1', value: 1 },
+      { key: 'key2', value: 2 },
+      { key: 'key3', value: 3 },
+    ]);
+  });
+
+  it('should iterate over all key value pairs', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    const result: any[] = [];
+
+    dictionary.forEach((key, value) => {
+      result.push({ key, value });
+    });
+
+    expect(result).toEqual([
+      { key: 'key1', value: 1 },
+      { key: 'key2', value: 2 },
+      { key: 'key3', value: 3 },
+    ]);
+  });
+
+  it('should stop iteration if callback returns false', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    const result: any[] = [];
+    dictionary.forEach((key, value) => {
+      result.push({ key, value });
+      return false;
+    });
+
+    expect(result).toEqual([{ key: 'key1', value: 1 }]);
+  });
+
+  it('should return the size of the dictionary', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    expect(dictionary.size()).toBe(3);
+  });
+
+  it('should clear the dictionary', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    dictionary.clear();
+    expect(dictionary.size()).toBe(0);
+  });
+
+  it('should return true if the dictionary is empty', () => {
+    expect(dictionary.isEmpty()).toBeTruthy();
+  });
+
+  it('should return false if the dictionary is not empty', () => {
+    dictionary.set('key1', 1);
+    expect(dictionary.isEmpty()).toBeFalsy();
+  });
+
+  it('should return an string representation of the dictionary', () => {
+    dictionary.set('key1', 1);
+    dictionary.set('key2', 2);
+    dictionary.set('key3', 3);
+    expect(dictionary.toString()).toBe('[#key1: 1][#key2: 2][#key3: 3]');
+  });
+
+  it('should return an empty string if the dictionary is empty', () => {
+    expect(dictionary.toString()).toBe('');
+  });
 });
--- src/structures/dictionary.ts
+++ src/structures/dictionary.ts
@@ -33,4 +33,54 @@ export class Dictionary<T> {
   get(key): T | undefined {
     return this.table[key] ? this.table[key].value : undefined;
   }
+
+  keyValues(): Array<ValuePair<T>> {
+    return Object.values(this.table);
+  }
+
+  keys(): string[] {
+    return this.keyValues().map(valuePair => valuePair.key);
+  }
+
+  values(): T[] {
+    return this.keyValues().map(valuePair => valuePair.value);
+  }
+
+  forEach(callbackFn): void {
+    const valuePairs = this.keyValues();
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
+
+      if (result === false) {
+        break;
+      }
+    }
+  }
+
+  size(): number {
+    return this.keyValues().length;
+  }
+
+  isEmpty(): boolean {
+    return this.size() === 0;
+  }
+
+  clear(): void {
+    this.table = {};
+  }
+
+  toString(): string {
+    if (this.isEmpty()) {
+      return '';
+    }
+
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+
+    for (let i = 1; i < valuePairs.length; i++) {
+      objString += valuePairs[i].toString();
+    }
+
+    return objString;
+  }
 }
